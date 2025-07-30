2f6157ce5307bb4298a1804909ddbed60d292c12
Davi Batista <davideosmar11@gmail.com>
2025-07-30 15:55:32 -0300

    refactor: changed logic on user findByUserName method

--- src/modules/user/domain/dtos/repositories/user.repository.ts
+++ src/modules/user/domain/dtos/repositories/user.repository.ts
@@ -3,8 +3,8 @@ import { User } from '../../../../../shared/infra/db/generated/prisma';
 export interface UserRepositoryInterface {
   encryptedFields: (keyof User)[];
   findById(id: string): Promise<Partial<User> | null>;
-  findByEmail(email: string): Promise<Partial<User>>;
-  findByPhoneNumber(phoneNumber: string): Promise<Partial<User>>;
-  findByUsername(username: string): Promise<Partial<User>>;
+  findByEmail(email: string): Promise<Partial<User> | null>;
+  findByPhoneNumber(phoneNumber: string): Promise<Partial<User> | null>;
+  findByUsername(username: string): Promise<Partial<User>[] | null>;
   setPassword(id: string, password: string): Promise<void>;
 }
--- src/modules/user/infra/db/repositories/user.repository.ts
+++ src/modules/user/infra/db/repositories/user.repository.ts
@@ -16,6 +16,7 @@ export class UserRepository implements UserRepositoryInterface{
         private encrypterProvider: EncrypterProvider,
     ){}
 
+    /* This method will find a single user using a given id */
     async findById(id: string): Promise<Partial<User> | null> {
         const user = await prisma.user.findUnique({
             where: {
@@ -24,6 +25,7 @@ export class UserRepository implements UserRepositoryInterface{
             select: {
                 id_user: true,
                 email: true,
+                phone_number: true,
                 name: true,
                 createdAt: true,
             }
@@ -38,5 +40,94 @@ export class UserRepository implements UserRepositoryInterface{
         )
 
         return decryptedUser;
+    }
+
+    /* This method will find a single user using a given email */
+    async findByEmail(email: string): Promise<Partial<User> | null> {
+        const encryptedEmail = this.encrypterProvider.encrypt({
+            content: email
+        });
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email: encryptedEmail
+            },
+            select: {
+                id_user: true,
+                email: true,
+                phone_number: true,
+                name: true,
+                createdAt: true,
+            }
+        })
+
+        if (!user) {
+            return null
+        }
+
+        const decryptedUser = this.encrypterProvider.decryptData(
+            user, this.encryptedFields as (keyof typeof user)[]
+        )
+
+        return decryptedUser;
+    }
+
+    /* This method will find a single user using a given phonenumber */
+    async findByPhoneNumber(phoneNumber: string): Promise<Partial<User> | null> {
+        const encryptedPhoneNumber = this.encrypterProvider.encrypt({
+            content: phoneNumber
+        });
+
+        const user = await prisma.user.findUnique({
+            where: {
+                phone_number: encryptedPhoneNumber
+            },
+            select: {
+                id_user: true,
+                email: true,
+                phone_number: true,
+                name: true,
+                createdAt: true,
+            }
+        })
+
+        if (!user) {
+            return null
+        }
+
+        const decryptedUser = this.encrypterProvider.decryptData(
+            user, this.encryptedFields as (keyof typeof user)[]
+        )
+
+        return decryptedUser;
+    }
+
+    /* This method will find a list of users given a string */
+    async findByUsername(username: string): Promise<Partial<User>[] | null> {
+
+        const users = await prisma.user.findMany({
+            where: {
+                name: {
+                    contains: username,
+                }
+            },
+            select: {
+                id_user: true,
+                email: true,
+                phone_number: true,
+                name: true,
+                createdAt: true,
+            }
+        })
+
+        if (!users) {
+            return null
+        }
+
+        const decryptedUsers = users.map(user => this.encrypterProvider.decryptData(
+            user, this.encryptedFields as (keyof typeof user)[]
+        ))
+
+        return decryptedUsers;
     }
 }
