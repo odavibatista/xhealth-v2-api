3bfa8921d8a1268c4de042a12255faa61429d93d
Davi Batista <davideosmar11@gmail.com>
2025-07-30 15:45:18 -0300

    feat: added findById method on user db repo, changed id keys of the entities

--- src/modules/user/domain/dtos/repositories/user.repository.ts
+++ src/modules/user/domain/dtos/repositories/user.repository.ts
@@ -2,7 +2,7 @@ import { User } from '../../../../../shared/infra/db/generated/prisma';
 
 export interface UserRepositoryInterface {
   encryptedFields: (keyof User)[];
-  findById(id: string): Promise<Partial<User>>;
+  findById(id: string): Promise<Partial<User> | null>;
   findByEmail(email: string): Promise<Partial<User>>;
   findByPhoneNumber(phoneNumber: string): Promise<Partial<User>>;
   findByUsername(username: string): Promise<Partial<User>>;
--- src/modules/user/infra/db/repositories/user.repository.ts
+++ src/modules/user/infra/db/repositories/user.repository.ts
@@ -1,1 +1,42 @@
-export class UserRepository {}
+import { Injectable } from "@nestjs/common";
+import { UserRepositoryInterface } from "../../../domain/dtos/repositories/user.repository";
+import { User } from "../../../../../shared/infra/db/generated/prisma";
+import { EncrypterProvider } from "../../../../../shared/infra/providers/Encrypter.provider";
+import { prisma } from "../../../../../shared/infra/db/prisma";
+
+@Injectable()
+export class UserRepository implements UserRepositoryInterface{
+    public encryptedFields: (keyof User)[] = [
+        'email',
+        'password',
+        "phone_number"
+    ];
+
+    constructor(
+        private encrypterProvider: EncrypterProvider,
+    ){}
+
+    async findById(id: string): Promise<Partial<User> | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                id_user: id
+            },
+            select: {
+                id_user: true,
+                email: true,
+                name: true,
+                createdAt: true,
+            }
+        })
+
+        if (!user) {
+            return null
+        }
+
+        const decryptedUser = this.encrypterProvider.decryptData(
+            user, this.encryptedFields as (keyof typeof user)[]
+        )
+
+        return decryptedUser;
+    }
+}
