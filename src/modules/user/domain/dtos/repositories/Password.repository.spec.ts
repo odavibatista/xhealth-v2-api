a3807683dc656f1446c59ff92805378e1a3637d4
Davi Batista <davideosmar11@gmail.com>
2025-08-02 17:25:32 -0300

    test(login-log+password-history): added interface test suites

--- src/modules/user/domain/dtos/repositories/Password.repository.spec.ts
+++ src/modules/user/domain/dtos/repositories/Password.repository.spec.ts
@@ -1,5 +1,24 @@
 import { PasswordRepositoryInterface } from './Password.repository';
 
-describe('PasswordRepository', () => {
-  it('should be defined', () => {});
+describe('Password Repository Interface Test Suites', () => {
+  const mockInterface: PasswordRepositoryInterface = {
+    encryptedFields: ['password'],
+    create: jest.fn(),
+    getByUserId: jest.fn(),
+  };
+
+  it('should have encryptedFields defined', () => {
+    expect(mockInterface.encryptedFields).toBeDefined();
+    expect(Array.isArray(mockInterface.encryptedFields)).toBe(true);
+  });
+
+  it('should have create method defined', () => {
+    expect(mockInterface.create).toBeDefined();
+    expect(typeof mockInterface.create).toBe('function');
+  });
+
+  it('should have getByUserId method defined', () => {
+    expect(mockInterface.getByUserId).toBeDefined();
+    expect(typeof mockInterface.getByUserId).toBe('function');
+  });
 });
