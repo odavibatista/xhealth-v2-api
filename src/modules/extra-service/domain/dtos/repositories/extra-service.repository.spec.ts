28963b8492dd274898de0c77f378ded591493de5
Davi Batista <davideosmar11@gmail.com>
2025-08-04 15:55:16 -0300

    test(gym-plan): added gym plan repository test suites for its existing methods

--- src/modules/extra-service/domain/dtos/repositories/extra-service.repository.spec.ts
+++ src/modules/extra-service/domain/dtos/repositories/extra-service.repository.spec.ts
@@ -4,6 +4,7 @@ describe('Extra Service Repository Interface Test Suites', () => {
   const mockInterface: ExtraServiceRepositoryInterface = {
     encryptedFields: [],
     findById: jest.fn(),
+    findAll: jest.fn(),
   };
 
   it('should have an encryptedFields property', () => {
@@ -12,5 +13,9 @@ describe('Extra Service Repository Interface Test Suites', () => {
 
   it('should have a findById method', () => {
     expect(typeof mockInterface.findById).toBe('function');
+  });
+
+  it('should have a findAll method', () => {
+    expect(typeof mockInterface.findAll).toBe('function');
   });
 });
