920bc857a8ed3efc60eb516b29adc84ef5e30ada
Davi Batista <davideosmar11@gmail.com>
2025-08-06 20:28:54 -0300

    feat(admin): added adming login usecase

--- src/modules/administrator/infra/usecases/admin-login.usecase.ts
+++ src/modules/administrator/infra/usecases/admin-login.usecase.ts
@@ -1,1 +1,45 @@
-export class AdminLoginUsecase {}
+import { Inject } from "@nestjs/common";
+import { UseCaseInterface } from "../../../../shared/domain/protocols/UseCase.protocol";
+import { AdministratorRepository } from "../db/repositories/administrator.repository";
+import { JWTProvider } from "../../../user/infra/providers/jwt.provider";
+import { AdminLoginRequestDTO, AdminLoginResponseDTO } from "../../domain/dtos/requests/AdminLogin.request.dto";
+import { InvalidCredentialsException } from "../../../user/domain/dtos/errors/InvalidCredentials.exception";
+import { UnprocessableDataException } from "../../../../shared/domain/errors/UnprocessableData.exception";
+
+export class AdminLoginUsecase implements UseCaseInterface  {
+      constructor(
+        @Inject()
+        private userRepository: AdministratorRepository,
+        private readonly jwtProvider: JWTProvider,
+      ) {}
+
+      async execute(data: AdminLoginRequestDTO): Promise<AdminLoginResponseDTO | InvalidCredentialsException | UnprocessableDataException> {
+        const admin = await this.userRepository.findByEmail(data.email);
+
+        if (!admin || !admin.id_administrator || !admin.name) throw new InvalidCredentialsException();
+
+        const isPasswordValid = await this.userRepository.comparePassword(
+            admin.id_administrator,
+            data.password,
+        )
+
+        if (!isPasswordValid) {
+          return new InvalidCredentialsException();
+        }
+
+        const token = this.jwtProvider.generate({
+            payload: {
+                id_user: admin.id_administrator,
+                name: admin.name,
+            }
+        })
+
+        return {
+          token,
+          admin: {
+            id_admin: admin.id_administrator,
+            name: admin.name,
+          },
+        };
+      }
+}
