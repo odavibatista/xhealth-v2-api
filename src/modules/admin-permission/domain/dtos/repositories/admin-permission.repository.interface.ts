cd333fb127d620e62ef592b0ac0bc9f0b3b917be
Davi Batista <davideosmar11@gmail.com>
2025-08-10 21:24:10 -0300

    feat(admin-permission): added methods on admin-permission repository

--- src/modules/admin-permission/domain/dtos/repositories/admin-permission.repository.interface.ts
+++ src/modules/admin-permission/domain/dtos/repositories/admin-permission.repository.interface.ts
@@ -1,12 +1,17 @@
 import { AdministratorPermission } from '@prisma/client';
 import { AdminAlreadyHasPermissionException } from '../errors/AdminAlreadyHasPermission.exception.dto';
+import { PermissionDoesNotExistException } from '../errors/PermissionDoesNotExist.exception.dto';
 
 export interface AdminPermissionRepositoryInterface {
   encryptedFields: (keyof AdministratorPermission)[];
+  findByAdminId(adminId: string): Promise<Partial<AdministratorPermission> | null>;
+
   addPermission(
     adminId: string,
     permission: string,
   ): Promise<boolean | AdminAlreadyHasPermissionException>;
-  // removePermission(adminId: string, permission: string): Promise<boolean>;
-  // hasPermission(adminId: string, permission: string): Promise<boolean>;
+  
+  removePermission(adminId: string, permission: string): Promise<boolean>;
+  
+  hasPermission(adminId: string, permission: string): Promise<boolean | PermissionDoesNotExistException>;
 }
--- src/modules/admin-permission/infra/db/repositories/admin-permission.repository.ts
+++ src/modules/admin-permission/infra/db/repositories/admin-permission.repository.ts
@@ -3,6 +3,7 @@ import { AdminPermissionRepositoryInterface } from '../../../domain/dtos/reposit
 import { prisma } from '../../../../../shared/infra/db/prisma';
 import { Injectable } from '@nestjs/common';
 import { AdminAlreadyHasPermissionException } from '../../../domain/dtos/errors/AdminAlreadyHasPermission.exception.dto';
+import { PermissionDoesNotExistException } from '../../../domain/dtos/errors/PermissionDoesNotExist.exception.dto';
 
 @Injectable()
 export class AdminPermissionRepository
@@ -12,6 +13,50 @@ export class AdminPermissionRepository
 
   constructor() {}
 
+  /* Buscar permissões por ID do administrador */
+  async findByAdminId(adminId: string): Promise<Partial<AdministratorPermission> | null> {
+    const adminPermission = await prisma.administratorPermission.findUnique({
+      where: {
+        id_administrator_permission: adminId,
+        deletedAt: null,
+      },
+      omit: {
+        created_at: true,
+        updated_at: true,
+      }
+    });
+
+    if (!adminPermission) return null;
+
+    return adminPermission;
+  }
+
+  /* Remover uma permissão */
+  async removePermission(adminId: string, permission: string): Promise<boolean> {
+    const adminPermission = await prisma.administratorPermission.findUnique({
+      where: {
+        id_administrator_permission: adminId,
+      },
+    });
+
+    if (!adminPermission) return false;
+
+    if (adminPermission[permission as keyof AdministratorPermission] === false)
+      return false;
+
+    await prisma.administratorPermission.update({
+      where: {
+        id_administrator_permission: adminId,
+      },
+      data: {
+        [permission]: false,
+      },
+    });
+
+    return true;
+  }
+
+  /* Adicionar uma permissão */
   async addPermission(adminId: string, permission: string): Promise<boolean> {
     const adminPermission = await prisma.administratorPermission.findUnique({
       where: {
@@ -37,5 +82,18 @@ export class AdminPermissionRepository
     });
 
     return true;
+  }
+
+  /* Verifica se o usuário possui a permissão */
+  async hasPermission(adminId: string, permission: string): Promise<boolean> {
+    const adminPermission = await prisma.administratorPermission.findUnique({
+      where: {
+        id_administrator_permission: adminId,
+      },
+    });
+
+    if (!adminPermission) throw new PermissionDoesNotExistException();
+
+    return adminPermission[permission as keyof AdministratorPermission] === true;
   }
 }
