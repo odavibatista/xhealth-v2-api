4bf0a286c8b270e3d1f28366b533b7f0cf40287a
Davi Batista <davideosmar11@gmail.com>
2025-08-11 10:03:35 -0300

    feat(city): added city seeder

--- src/shared/infra/db/prisma/seeders/index.ts
+++ src/shared/infra/db/prisma/seeders/index.ts
@@ -6,11 +6,13 @@ import { administratorSeeder } from './administrator.seed';
 import { adminPermissionSeeder } from './admin-permission.seed';
 import { trainerSeeder } from './trainer.seed';
 import ufsSeeder from './uf.seed';
+import citySeeder from './city.seed';
 
 const seed = async () => {
   console.log('Running seed: ');
   
   await ufsSeeder();
+  await citySeeder();
   await administratorSeeder();
   await adminPermissionSeeder();
   await trainerSeeder();
