5eab34c291a98a9d29d1cd3cf698e28ea92aeded
Davi Batista <davideosmar11@gmail.com>
2025-08-10 21:19:47 -0300

    fix(admin-permission): fixed logic on seeding

--- src/shared/infra/db/prisma/seeders/admin-permission.seed.ts
+++ src/shared/infra/db/prisma/seeders/admin-permission.seed.ts
@@ -1,50 +1,55 @@
 import { prisma } from '..';
+import { EncrypterProvider } from '../../../providers/Encrypter.provider';
 
 export const adminPermissionSeeder = async () => {
+  console.log('Running admin permission seeder...');
   const adminName = 'Admin Default';
+  const encrypterProvider = new EncrypterProvider();
 
-    const existingAdmin = await prisma.administrator.findFirst({
-        where: {
-        name: adminName,
-        },
-    });
+  const enc = encrypterProvider.encrypt({ content: adminName });
 
-    if (!existingAdmin) return;
+  const existingAdmin = await prisma.administrator.findFirst({
+    where: {
+      name: enc,
+    },
+  });
 
-    await prisma.administratorPermission.create({
-        data: {
-            administrator_id: existingAdmin.id_administrator,
-            can_create_gyms: true,
-            can_edit_gyms: true,
-            can_delete_gyms: true,
+  if (!existingAdmin) return;
 
-            can_create_users: true,
-            can_edit_users: true,
-            can_delete_users: true,
+  await prisma.administratorPermission.create({
+    data: {
+      administrator_id: existingAdmin.id_administrator,
+      can_create_gyms: true,
+      can_edit_gyms: true,
+      can_delete_gyms: true,
 
-            can_create_trainers: true,
-            can_edit_trainers: true,
-            can_delete_trainers: true,
+      can_create_users: true,
+      can_edit_users: true,
+      can_delete_users: true,
 
-            can_create_extra_services: true,
-            can_edit_extra_services: true,
-            can_delete_extra_services: true,    
+      can_create_trainers: true,
+      can_edit_trainers: true,
+      can_delete_trainers: true,
 
-            can_create_sport_modalities: true,
-            can_edit_sport_modalities: true,
-            can_delete_sport_modalities: true,
+      can_create_extra_services: true,
+      can_edit_extra_services: true,
+      can_delete_extra_services: true,
 
-            can_create_testimonies: true,
-            can_edit_testimonies: true,
-            can_delete_testimonies: true,
+      can_create_sport_modalities: true,
+      can_edit_sport_modalities: true,
+      can_delete_sport_modalities: true,
 
-            can_create_gym_plans: true,
-            can_edit_gym_plans: true,
-            can_delete_gym_plans: true,
+      can_create_testimonies: true,
+      can_edit_testimonies: true,
+      can_delete_testimonies: true,
 
-            can_create_administrators: true,
-            can_edit_administrators: true,
-            can_delete_administrators: true,
-        },
-    })
-}
+      can_create_gym_plans: true,
+      can_edit_gym_plans: true,
+      can_delete_gym_plans: true,
+
+      can_create_administrators: true,
+      can_edit_administrators: true,
+      can_delete_administrators: true,
+    },
+  });
+};
