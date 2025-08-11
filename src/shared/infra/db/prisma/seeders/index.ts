db37bf1c8cc689155bc3f4207b0a7c690475e005
Davi Batista <davideosmar11@gmail.com>
2025-08-10 21:12:37 -0300

    feat(admin-permission): added seed for administrator permission

--- src/shared/infra/db/prisma/seeders/index.ts
+++ src/shared/infra/db/prisma/seeders/index.ts
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import { appConfigurations } from '../../../../config/app.config';
 import { mainSeeder } from './main.seed';
 import { administratorSeeder } from './administrator.seed';
+import { adminPermissionSeeder } from './admin-permission.seed';
 
 const seed = async () => {
   console.log('Running seed: ');
 
   await administratorSeeder();
+  await adminPermissionSeeder();
 };
 
 const seedTest = async () => {
