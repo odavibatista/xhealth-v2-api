dd59869c385d2ce947cd5508c4a59abc6cab401b
Davi Batista <davideosmar11@gmail.com>
2025-08-12 16:58:24 -0300

    test(gym): added automated tests for delete gym usecase

--- src/modules/address/infra/db/repositories/address.repository.spec.ts
+++ src/modules/address/infra/db/repositories/address.repository.spec.ts
@@ -5,72 +5,66 @@ describe('Address Repository Test Suites', () => {
   let repository: AddressRepository;
   let encrypterProvider: EncrypterProvider;
 
-    beforeEach(() => {
-      jest.useFakeTimers({ doNotFake: ['nextTick'] });
-    });
-  
-    beforeEach(() => {
-      encrypterProvider = new EncrypterProvider();
-      repository = new AddressRepository(encrypterProvider);
-    });
-  
-    afterEach(() => {
-      jest.clearAllMocks();
-    });
-  
-    afterAll(() => {
-      jest.useRealTimers();
-    });
+  beforeEach(() => {
+    jest.useFakeTimers({ doNotFake: ['nextTick'] });
+  });
 
-    const mockAddress = {
-      id_address: 'valid-address-id',
-      cep: '12345678',
-      street: 'Rua Exemplo',
-      number: '123',
-      complement: 'Apto 1',
-      city: 'Cidade Exemplo',
-      uf_id: 'SP',
-    };
+  beforeEach(() => {
+    encrypterProvider = new EncrypterProvider();
+    repository = new AddressRepository(encrypterProvider);
+  });
 
-    describe('findById method test suites', () => {
-      it('should return an address if found', async () => {
-        jest
-          .spyOn(repository, 'findById')
-          .mockResolvedValueOnce(mockAddress);
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-        const result = await repository.findById(mockAddress.id_address);
+  afterAll(() => {
+    jest.useRealTimers();
+  });
 
-        expect(result).toEqual(mockAddress);
-      });
+  const mockAddress = {
+    id_address: 'valid-address-id',
+    cep: '12345678',
+    street: 'Rua Exemplo',
+    number: '123',
+    complement: 'Apto 1',
+    city: 'Cidade Exemplo',
+    uf_id: 'SP',
+  };
 
-      it('should return null if not found', async () => {
-        jest
-          .spyOn(repository, 'findById')
-          .mockResolvedValueOnce(null);
+  describe('findById method test suites', () => {
+    it('should return an address if found', async () => {
+      jest.spyOn(repository, 'findById').mockResolvedValueOnce(mockAddress);
 
-        const result = await repository.findById('invalid-id');
+      const result = await repository.findById(mockAddress.id_address);
 
-        expect(result).toBeNull();
-      });
+      expect(result).toEqual(mockAddress);
     });
 
-    
+    it('should return null if not found', async () => {
+      jest.spyOn(repository, 'findById').mockResolvedValueOnce(null);
 
-    describe('delete method test suites', () => {
-      it('should return true when address is successfully deleted', async () => {
-        jest.spyOn(repository, 'delete').mockResolvedValueOnce(true);
+      const result = await repository.findById('invalid-id');
 
-        const result = await repository.delete(mockAddress.id_address);
-
-        expect(result).toBe(true);
-      });
-
-      it('should return false when address deletion fails', async () => {
-        jest.spyOn(repository, 'delete').mockResolvedValueOnce(false);
-
-        const result = await repository.delete('invalid-id');
-
-        expect(result).toBe(false);
-      });
+      expect(result).toBeNull();
     });
+  });
+
+  describe('delete method test suites', () => {
+    it('should return true when address is successfully deleted', async () => {
+      jest.spyOn(repository, 'delete').mockResolvedValueOnce(true);
+
+      const result = await repository.delete(mockAddress.id_address);
+
+      expect(result).toBe(true);
+    });
+
+    it('should return false when address deletion fails', async () => {
+      jest.spyOn(repository, 'delete').mockResolvedValueOnce(false);
+
+      const result = await repository.delete('invalid-id');
+
+      expect(result).toBe(false);
+    });
+  });
 });
