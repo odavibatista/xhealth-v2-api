cdb667ce073ebda58060d53ccac53883d1af7997
Davi Batista <davideosmar11@gmail.com>
2025-08-13 09:32:11 -0300

    test(e2e): added basic e2e tests for admin permissions' endpoints

--- src/modules/admin-permission/http/controllers/admin-permission.controller.spec.ts
+++ src/modules/admin-permission/http/controllers/admin-permission.controller.spec.ts
@@ -1,20 +1,121 @@
+import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminPermissionController } from './admin-permission.controller';
+import { PrismaProvider } from '../../../../shared/infra/providers/Prisma.provider';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../../../../app/app.module';
+import { SharedModule } from '../../../../shared/infra/modules/Shared.module';
+import { Environment } from '../../../../shared/config/app.config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { administratorSeeder } from '../../../../shared/infra/db/prisma/seeders/administrator.seed';
+import { adminPermissionSeeder } from '../../../../shared/infra/db/prisma/seeders/admin-permission.seed';
+import { ChangePermissionRequestDTO } from '../../domain/dtos/requests/ChangePermission.request.dto';
+import { AddPermissionRequestDTO } from '../../domain/dtos/requests/AddPermission.request.dto';
+import { NotAuthenticatedException } from '../../../../shared/domain/errors/NotAuthenticated.exception';
+import { RemovePermissionRequestDTO } from '../../domain/dtos/requests/RemovePermission.request.dto';
 
-describe('AdminPermissionController', () => {
+describe('Admin Permissions Controller - /permissions', () => {
+  let controllerRoute = '/permissions';
+  let addPermissionRoute = `${controllerRoute}/add`;
+  let changePermissionRoute = `${controllerRoute}/change`;
+  let removePermissionRoute = `${controllerRoute}/remove`;
+
   let controller: AdminPermissionController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AdminPermissionController],
-    }).compile();
+  let app: INestApplication;
+  let prisma: PrismaProvider;
+  let jwtToken: string;
 
-    controller = module.get<AdminPermissionController>(
-      AdminPermissionController,
-    );
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, SharedModule],
+      providers: [PrismaProvider],
+    })
+      .overrideProvider(PrismaProvider)
+      .useValue(new PrismaProvider(Environment.TEST))
+      .compile();
+
+    prisma = moduleRef.get(PrismaProvider);
+
+    app = moduleRef.createNestApplication<NestExpressApplication>();
+    app.useGlobalFilters();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    await prisma.seed([administratorSeeder, adminPermissionSeeder]);
+    jest.clearAllMocks();
+  });
+
+  afterEach(async () => {
+    await prisma.clear('all');
+  });
+
+  describe('POST /permissions/add', () => {
+    let body: AddPermissionRequestDTO;
+
+    it('should return 401 and NotAuthenticatedException if no token is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .post(addPermissionRoute)
+        .send({
+          admin: 'admin-cuid',
+          permission: 'new-permission',
+        })
+        .expect(401)
+        .set('Accept', 'application/json');
+
+      expect(response.body).toHaveProperty('statusCode', 401);
+      expect(response.body).toHaveProperty(
+        'message',
+        new NotAuthenticatedException().message,
+      );
+    });
+  });
+
+  describe('POST /permissions/change', () => {
+    let body: ChangePermissionRequestDTO;
+
+    it('should return 401 and NotAuthenticatedException if no token is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .post(changePermissionRoute)
+        .send({
+          admin: 'admin-cuid',
+          permission: 'existing-permission',
+          newPermission: 'new-permission',
+        })
+        .expect(401)
+        .set('Accept', 'application/json');
+
+      expect(response.body).toHaveProperty('statusCode', 401);
+      expect(response.body).toHaveProperty(
+        'message',
+        new NotAuthenticatedException().message,
+      );
+    });
+  });
+
+  describe('POST /permissions/remove', () => {
+    let body: RemovePermissionRequestDTO;
+
+    it('should return 401 and NotAuthenticatedException if no token is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .post(removePermissionRoute)
+        .send({
+          admin: 'admin-cuid',
+          permission: 'permission-to-remove',
+        })
+        .expect(401)
+        .set('Accept', 'application/json');
+
+      expect(response.body).toHaveProperty('statusCode', 401);
+      expect(response.body).toHaveProperty(
+        'message',
+        new NotAuthenticatedException().message,
+      );
+    });
   });
 });
